a846011ff15a5a01721c84a7d0b04be357b8abd6
Poirot92 <upirimqulov@gmail.com>
2026-04-26 22:40:02 +0500

    yehey

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to My Project by Poirot 92 "yehe blyat"</h1>;
+	return <h1>Welcome to My Project by Poirot 92 yehe blyat</h1>;
 }
