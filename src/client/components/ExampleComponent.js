50517bb771edaa885aceea1ff23cab2f9a1dc985
Poirot92 <upirimqulov@gmail.com>
2026-04-27 09:05:16 +0500

    qani endi

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1> Welcome to My Project by Poirot 92 yehe blyat yaxoo</h1>;
+	return <h1> Welcome to My Project by yehe blyat yaxoo</h1>;
 }
