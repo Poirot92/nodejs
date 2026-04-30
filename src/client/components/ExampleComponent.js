9691775fc0efc76cf470eb5b11a596d81b6208e0
Poirot92 <upirimqulov@gmail.com>
2026-04-30 21:48:45 +0500

    test

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1> Welcome to My Project by normalni yozsang bolmayma?? ороролр </h1>;
+	return <h1> Welcome to My Project by normalni yozsang bolmayma?? 123123123 </h1>;
 }
